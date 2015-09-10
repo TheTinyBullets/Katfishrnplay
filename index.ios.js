3d6658f5c113256a2914ec2d3a19890975f57c80
ChristopherSalam <christopher.salam@gmail.com>
2015-09-10 12:28:55 -0700

    Changes made to file to remove login

--- index.ios.js
+++ index.ios.js
@@ -3,27 +3,13 @@
 
 var React = require('react-native');
 //var FB = require('fb');
-var FBSDKLogin = require('react-native-fbsdklogin');
-var FBSDKCore = require('react-native-fbsdkcore');
-var Login = require('./JS/Login')
-
-//**********************From this point until the next is broken core login
-
-// var params = {
-//   userId: '123456789'
-// }
-// var token = "CAAGZCY3FYPOMBAEsXn8A7L1ZC9jm7ZAR5FwJ4LIhQjeyQuCme…JM9Di9Q4CnZCDZCxPbco14sFnkkpq1fjQuLEPB5o11TGBVEZD"
-// var ver = 'v2.4'
-//Create a graph request asking for friends with a callback to handle the response.
-//FBSDKGraphRequestManager.batchRequests([profileRequest], function() {}, 60);
-// <----------- insert the function here fool
-// Start the graph request.
-//
-
-// console.log(FBSDKGraphRequest)
-
-//**********************************This point
-
+// var FBSDKLogin = require('react-native-fbsdklogin');
+// var FBSDKCore = require('react-native-fbsdkcore');
+var Login = require('./js/Login');
+var person = require('./js/PersonDB');
+var Featured = require('./js/Featured');
+var Search = require('./js/Search');
+this.tokenString;
 
 var {
   AppRegistry,
@@ -34,39 +20,48 @@ var {
   TabBarIOS
 } = React;
 
-var {
-  FBSDKLoginButton,
-} = FBSDKLogin;
+// var {
+//   FBSDKLoginButton,
+// } = FBSDKLogin;
 
-var {
-  FBSDKGraphRequest,
-  FBSDKGraphRequestManager,
-  FBSDKAccessToken
-} = FBSDKCore;
+// var {
+//   FBSDKGraphRequest,
+//   FBSDKGraphRequestManager,
+//   FBSDKAccessToken
+// } = FBSDKCore;
 
-//var Feed = require('./JS/Feed');
-var Featured = require('./JS/Featured');
-var Search = require('./JS/Search');
+// var fetchMyId = new FBSDKGraphRequest((error, result) => {
+//  if (error) {
+//    console.log('Error making request.', error);
+//  } else {
+//   console.log('FB id received')
+//  }
+// }, '/me', {}, this.tokenString, "v2.4", "GET");
 
-var fetchFriendsRequest = new FBSDKGraphRequest((error, result, params, token, ) => {
-  if (error) {
-    alert('Error making request.');
-  } else {
-    console.log('result from the graph req',result)
-  }
-}, '/me/friends', {}, 1234567890, 'v2.4', 'GET');
-//fetchFriendsRequest.start();
+// var fetchFriendsRequest = new FBSDKGraphRequest((error, result) => {
+//  if (error) {
+//    console.log('Error making request.', error);
+//  } else {
+//    require('./js/PersonDB').fish(result.data);
+//  }
+// }, '/me/friends', {}, this.tokenString, "v2.4", "GET");
+// //fetchFriendsRequest.start();
 
+// FBSDKGraphRequestManager.batchRequests([fetchFriendsRequest, fetchMyId], function (error) {
+//  if (error) {
+//    console.log("ERROR", error);
+//  }
+// }, 60);
 
 var Katfish = React.createClass({
 
   render: function() {
     window.Katfish = this;
-    if(!this.tokenString){
+    if(!this.state){
       this.getToken();
       return (
         <Image
-        source={{uri: 'http://q8italk.com/wp-content/uploads/2014/10/Green-Starry-iPhone-6-plus-wallpaper-ilikewallpaper_com.png'}}
+        source={{uri: 'http://chrissalam.com/bash/sailing.png'}}
         style={styles.loginImage}>
         <Login
         style={styles.loginContainer}/>
@@ -74,32 +69,22 @@ var Katfish = React.createClass({
         );
     }
     else{
-      return require('./JS/TabBar')();
+      return require('./js/TabBar')();
     }
   },
   getToken: function(){
-    FBSDKAccessToken.getCurrentAccessToken((token) => {
-      if (token) {
-        // A non-null token indicates that the user is currently logged in.
-        console.log('TOKEN: ', token);
-        this.tokenString = token.tokenString;
-        this.userID = token.userID;
-      }else console.log('there is no token friend')
-    });
-    //this.getImage();
+    this.userID = person.ID
+    // FBSDKAccessToken.getCurrentAccessToken((token) => {
+    //   if (token) {
+    //     // A non-null token indicates that the user is currently logged in.
+    //     console.log('TOKEN: ', token);
+    //     this.tokenString = token.tokenString;
+    //     this.userID = token.userID;
+    //   } else console.log('there is no token')
+    // });
   },
-
-  // getImage: function(){
-  //   FB.api('/me/picture', 'get', function(response){
-  //     if (!response || response.error) {
-  //       alert('Error occured');
-  //     } else {
-  //       console.log(response);
-  //     }
-  //   })
-  // }
 });
 
-var styles = require('./JS/styles.js');
+var styles = require('./js/styles.js');
 
 AppRegistry.registerComponent('Katfish', () => Katfish);
