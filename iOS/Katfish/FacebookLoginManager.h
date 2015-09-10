dced55492b6e93f73b43ae559be9bb9b6195546b
ChristopherSalam <christopher.salam@gmail.com>
2015-09-10 12:37:31 -0700

    Adding back in the iOS folders

---
+++ iOS/Katfish/FacebookLoginManager.h
@@ -0,0 +1,18 @@
+//
+//  FacebookLoginManager.h
+//  Katfish
+//
+//  Created by Christopher Salam on 9/10/15.
+//  Copyright (c) 2015 Facebook. All rights reserved.
+//
+
+#ifndef Katfish_FacebookLoginManager_h
+#define Katfish_FacebookLoginManager_h
+
+
+#endif
+
+#import "RCTBridgeModule.h"
+
+@interface FacebookLoginManager : NSObject <RCTBridgeModule>
+@end
